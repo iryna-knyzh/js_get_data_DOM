970dd5fd26994697a1047de2c06c3ea7f0e322c5
Iryna Knyzh <iryna.knyzh@gmail.com>
2025-10-23 11:42:49 +0300

    make build

---
+++ js_get_data_DOM.f75de5e1.js
@@ -0,0 +1,12 @@
+'use strict';
+const elements = [
+    ...document.querySelectorAll('span.population')
+];
+const total = elements.reduce((sum, x)=>{
+    return sum + Number(x.textContent.replaceAll(',', ''));
+}, 0);
+const average = Math.floor(total / elements.length);
+document.querySelector('.total-population').textContent = total.toLocaleString();
+document.querySelector('.average-population').textContent = average.toLocaleString();
+
+//# sourceMappingURL=js_get_data_DOM.f75de5e1.js.map
